c727aaa42e7f2b6795d7f1e2e96b2a9f561ec5ac
prans8 <pranjalshandilya520@gmail.com>
2026-05-04 14:28:38 +0530

    final fix api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://team-task-manager-production.up.railway.app"
 });
 
 export default API;
